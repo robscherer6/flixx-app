83e77a50a339ceda41c517474c406db037d785e2
Rob Scherer <robscherer6@gmail.com>
2023-07-02 10:12:17 -0400

    able to display popular movies using async await and fetch to TMDB API

--- js/script.js
+++ js/script.js
@@ -2,13 +2,30 @@ const global = {
   currentPage: window.location.pathname
 };
 
+async function displayPopularMovies() {
+  const results = await fetchAPIData('movie/popular');
+  console.log(results);
+}
+
+// Fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = 'cf0142b5cbc8938b61594d502443f4c4';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-us`);
+
+  const data = await response.json();
+
+  return data;
+}
+
 // Highlight Active Link
 
 function highlightActiveLink () {
   const links = document.querySelectorAll('.nav-link');
   links.forEach((link) => {
     if (link.getAttribute('href') === global.currentPage) {
-      link.classList.add('active')
+      link.classList.add('active');
     }
   })
 }
@@ -18,7 +35,7 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      console.log('Home');
+      displayPopularMovies();
       break;
     case '/shows.html':
       console.log('Shows');
