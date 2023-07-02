daa81d53eb868823c1e0bf81f56e57464040cbba
Rob Scherer <robscherer6@gmail.com>
2023-07-02 10:49:13 -0400

    displaying top-20 popular movies

--- js/script.js
+++ js/script.js
@@ -3,8 +3,33 @@ const global = {
 };
 
 async function displayPopularMovies() {
-  const results = await fetchAPIData('movie/popular');
-  console.log(results);
+  const { results } = await fetchAPIData('movie/popular');
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="movie-details.html?id=${movie.id}">
+            ${
+              movie.poster_path ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="Movie Title"
+            />` : `<img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${movie.title}"
+          />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+          `;
+    document.getElementById('popular-movies').appendChild(div);
+  });
 }
 
 // Fetch data from TMDB API
