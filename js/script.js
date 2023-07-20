641e507080b40f79c7d9e5fd97fbfbd2410fed04
Rob Scherer <robscherer6@gmail.com>
2023-07-20 07:25:06 -0400

    added tv shows to display, similar to movies

--- js/script.js
+++ js/script.js
@@ -32,6 +32,37 @@ async function displayPopularMovies() {
   });
 }
 
+// Display 20 popular tv shows
+async function displayPopularShows() {
+  const { results } = await fetchAPIData('tv/popular');
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="tv-details.html?id=${show.id}">
+            ${
+              show.poster_path ? `<img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="{show.name}"
+            />` : `<img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${show.name}"
+          />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Air Date: ${show.first_air_date}</small>
+            </p>
+          </div>
+          `;
+    document.getElementById('popular-shows').appendChild(div);
+  });
+}
+
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
 
@@ -76,7 +107,7 @@ function init() {
       displayPopularMovies();
       break;
     case '/shows.html':
-      console.log('Shows');
+      displayPopularShows();
       break;
     case '/movie-details.html':
       console.log('Movie Details');
