4eba7898b41a56a9b65ce4ca3fde477a6f5e549e
Rob Scherer <robscherer6@gmail.com>
2023-08-13 17:05:18 -0400

    search now functional, pagination next

--- js/script.js
+++ js/script.js
@@ -230,12 +230,44 @@ async function search() {
     const { results, total_pages, page } = await searchAPIData();
 
     if (results === 0) {
-      showAlert('')
+      showAlert('No results found');
+      return;
     }
+    displaySearchResults(results);
+    document.querySelector('#search-term').value = '';
   } else {
     // alert no search term
-    showAlert('Please enter a search term', 'error');
+    showAlert('Please enter a search term');
   }
+}
+
+function displaySearchResults(results) {
+  results.forEach((result) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="${global.search.type}-details.html?id=${result.id}">
+            ${
+              result.poster_path ? `<img
+              src="https://image.tmdb.org/t/p/w500/${result.poster_path}"
+              class="card-img-top"
+              alt="${global.search.type === 'movie' ? result.title : result.name}"
+            />` : `<img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${global.search.type === 'movie' ? result.title : result.name}"
+          />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${global.search.type === 'movie' ? result.title : result.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${global.search.type === 'movie' ? result.release_date : result.first_air_date}</small>
+            </p>
+          </div>
+          `;
+    document.querySelector('#search-results').appendChild(div);
+  });
 }
 
 // Display Slider Movies
@@ -338,7 +370,7 @@ function highlightActiveLink () {
 }
 
 // Show Alert
-function showAlert(message, className) {
+function showAlert(message, className = 'error') {
   const alertEl = document.createElement('div');
   alertEl.classList.add('alert', className);
   alertEl.appendChild(document.createTextNode(message));
